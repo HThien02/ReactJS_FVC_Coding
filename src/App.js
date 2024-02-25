275fc8e0028c53fb2ea099b6fd772be63b8d8a4e
HThien02 <hieuthien2k2@gmail.com>
2024-02-25 19:26:04 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Staff from './component/Staff';
+import StaffList from './component/StaffListComponent';
+import { STAFFS } from './shared/staffs';
+import { useState } from 'react';
 function App() {
+  let [clickedStaff, setClickedStaff] = useState();
+
+
+  let staffs = STAFFS.map((staff) => {
+    function StaffClicked(){
+      setClickedStaff(staff);
+    }
+    return (
+      <Staff key={staff.id} staff={staff} onClick={StaffClicked}></Staff>
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StaffList staffs={staffs}></StaffList>
+      <div>{clickedStaff?.id}</div>
+      <div>{clickedStaff?.name}</div>
+      <div>{clickedStaff?.doB}</div> 
+      <div>{clickedStaff?.salaryScale}</div>
+      <div>{clickedStaff?.startDate}</div>
+      <div>{clickedStaff?.departmentId}</div>
+      <div>{clickedStaff?.annualLeave}</div>
+      <div>{clickedStaff?.overTime}</div>
+      <div><img src={clickedStaff?.image}/></div>
     </div>
   );
 }
